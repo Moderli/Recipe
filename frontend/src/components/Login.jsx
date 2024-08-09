61c1168056f2b6496931920a4069e0eef0f77f81
Moderli <99080776+Moderli@users.noreply.github.com>
2024-08-09 20:53:05 +0530

    Update Login.jsx

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -12,7 +12,7 @@ const Login = () => {
     const handleSubmit = (event) => {
         event.preventDefault();
         
-        axios.post( 'https://recipe-s41u.vercel.app/login', {email, password})
+        axios.post( 'https://recipe-lbts.onrender.com/login', {email, password})
         .then(result => {
             console.log(result);
             if(result.data === "Success"){
